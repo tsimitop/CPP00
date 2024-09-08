c8a3c89a31305fbff739d69e70e47f6dcec05375
Thaleia Simitopoulou <tsimitop@1-E-3.42heilbronn.de>
2024-09-08 17:53:47 +0200

    initial-constructor and end-destructor mesagges are correct

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,75 +1,87 @@
 #include "Account.hpp"
 #include <iostream>
+#include <ctime>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
 
 int	Account::getNbAccounts( void )
 {
-	std::cout << "FUNCTION DECLARED" << std::endl;
-	return (0);
+	return (_nbAccounts);
 }
 
 int	Account::getTotalAmount( void )
 {
-	std::cout << "FUNCTION DECLARED" << std::endl;
-	return (0);
+	return (_totalAmount);
 }
 
 int	Account::getNbDeposits( void )
 {
-	std::cout << "FUNCTION DECLARED" << std::endl;
-	return (0);
+	return (_totalNbDeposits);
 }
 
 int	Account::getNbWithdrawals( void )
 {
-	std::cout << "FUNCTION DECLARED" << std::endl;
-	return (0);
+	return (_totalNbWithdrawals);
 }
 
 void	Account::displayAccountsInfos( void )
 {
-	std::cout << "FUNCTION DECLARED" << std::endl;
+	_displayTimestamp();
+	std::cout << " accounts:" << _nbAccounts << ";total:" << _totalAmount << ";deposits:" << _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals << std::endl;
 }
 
 void		Account::makeDeposit( int deposit )
 {
 	(void)deposit;
-	std::cout << "FUNCTION DECLARED" << std::endl;
+	// std::cout << "FUNCTION DECLARED" << std::endl;
 }
 
 bool		Account::makeWithdrawal( int withdrawal )
 {
 	(void)withdrawal;
-	std::cout << "FUNCTION DECLARED" << std::endl;
+	// std::cout << "FUNCTION DECLARED" << std::endl;
 	return (true);
 }
 
 int			Account::checkAmount( void ) const
 {
-	std::cout << "FUNCTION DECLARED" << std::endl;
+	// std::cout << "FUNCTION DECLARED" << std::endl;
 	return (0);
 }
 
 void		Account::displayStatus( void ) const
 {
-	std::cout << "FUNCTION DECLARED" << std::endl;
+	// std::cout << "FUNCTION DECLARED" << std::endl;
 }
 
 void	Account::_displayTimestamp( void )
 {
-	std::cout << "FUNCTION DECLARED" << std::endl;
+	time_t	timestamp;
+	char	printable[20];
+
+	time(&timestamp);
+	strftime(printable, 20, "[%Y%m%d_%H%M%S]", localtime(&timestamp));
+	std::cout << printable;
 }
-
-
-
-
 
 Account::Account(int initial_deposit)
 {
-	std::cout << "Account created." << std::endl;
-	std::cout << "Account's initial_deposit is: " << initial_deposit << std::endl;
+	_totalAmount += initial_deposit;
+	this->_amount = initial_deposit;
+	this->_accountIndex = _nbAccounts;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex << ";amount:" << initial_deposit << ";created" << std::endl;
+	_nbAccounts++;
 }
 
 Account::~Account()
 {
-	std::cout << "Account destroyed." << std::endl;
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex << ";amount:" << this->_amount << ";closed" << std::endl;
 }
